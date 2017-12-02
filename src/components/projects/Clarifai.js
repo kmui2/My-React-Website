e7119924cca9293ad6c4d27da2d289968f786c8b
kmui2 <kmui2@wisc.edu>
2017-12-02 15:12:53 -0600

    read in file into bytes

--- src/components/projects/Clarifai.js
+++ src/components/projects/Clarifai.js
@@ -4,6 +4,7 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import { Link } from 'react-router-dom';
 import ClarifaiLib from 'clarifai';
+import $ from 'jquery';
 const app = new ClarifaiLib.App({
 	apiKey: 'fe578d618cf24fcc87df096b29b1d41f'
 });
@@ -14,13 +15,32 @@ export default class Clarifai extends React.Component {
 		this.state = {colors:[], link: 'http://kamui.ml:8000'};
 	}
 	componentDidMount = () => {
-		app.models.predict("eeed0b6733a644cea07cf4c60f87ebb7", "https://samples.clarifai.com/metro-north.jpg").then(
-			this.handleResponse,
-			function(err) {
-			  // there was an error
-			  console.log(err);
+		var handleResponse = this.handleResponse;
+		function readFile() {
+			
+			if (this.files && this.files[0]) {
+				
+				var FR= new FileReader();
+				
+				FR.addEventListener("load", function(e) {
+					document.getElementById("img").src       = e.target.result;
+					document.getElementById("b64").innerHTML = e.target.result;
+					app.models.predict("eeed0b6733a644cea07cf4c60f87ebb7", e.target.result.split(',')[1]).then(
+						handleResponse,
+						function(err) {
+						  // there was an error
+						  console.log(err);
+						}
+					  );
+					// console.log(e.target.result.split(',')[1])
+			  }); 
+			  
+			  FR.readAsDataURL( this.files[0] );
 			}
-		  );
+			
+		  }
+		  
+		  document.getElementById("inp").addEventListener("change", readFile);
 	}
 
 	handleResponse = (response) => {
@@ -50,6 +70,9 @@ export default class Clarifai extends React.Component {
 				<Grid fluid className="container">
 					Welcome to clarifai component
 					<a href={this.state.link} onClick={this.handleClick}>Click me!</a>
+					<input id="inp" type='file' />
+					<p id="b64"></p>
+					<img id="img" height="150"/>
 				</Grid>
 			</div>
 		);
